c1e0aaca97f38791f40af7a027c6e4e3287fb35e
Miguel Aragón <104454817+miguelaragonh@users.noreply.github.com>
2024-06-20 10:43:42 -0600

    upda

--- src/db/config.js
+++ src/db/config.js
@@ -1,12 +1,12 @@
 const { Sequelize} = require("sequelize");
 require("dotenv").config();
 module.exports = new Sequelize(
-  process.env.DB_DATABASE || "sgidb",
-  process.env.DB_USERNAME || "root",
-  process.env.DB_PASSWORD || null,
+  process.env.MYSQL_DATABASE || "sgidb",
+  process.env.MYSQLUSER || "root",
+  process.env.MYSQLPASSWORD || null,
   {
-    host: process.env.DB_HOST || "127.0.0.1",
+    host: process.env.MYSQLHOST || "127.0.0.1",
     dialect: process.env.DB_DIALECT || "mysql",
-    port: process.env.DB_PORT || 3306
+    port: process.env.MYSQLPORT|| 3306
   }
 ); 
