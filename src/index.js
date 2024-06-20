8abf878964135b5c4b1a5052c3dcc82587ea4775
Miguel Aragón <104454817+miguelaragonh@users.noreply.github.com>
2024-06-19 22:15:18 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const app = require('./app/app')
-
+const sequelize = require('../src/db/config'); 
 const port = process.env.PORT||4000;
 
 app.listen(port, ()=>
   {
 console.log(`Escuchando puerto ${port}`)
-console.log(Date())
+sequelize.authenticate()
+  .then(() => {
+    console.log('Conexión establecida exitosamente.');
+  })
+  .catch(err => {
+    console.error('No se pudo conectar a la base de datos:', err);
+  });
   } );
