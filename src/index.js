13c15823b3edee6eb3da54f443f85a0264a367e6
Miguel Aragón <104454817+miguelaragonh@users.noreply.github.com>
2024-06-19 22:48:49 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const app = require('./app/app')
 const sequelize = require('../src/db/config'); 
 const port = process.env.PORT||4000;
+require("dotenv").config();
 
 app.listen(port, ()=>
   {
 console.log(`Escuchando puerto ${port}`)
+
+console.log(process.env.DB_DATABASE);
+console.log(process.env.DB_USERNAME);
+console.log(process.env.DB_PASSWORD);
+console.log(process.env.DB_HOST);
+console.log(process.env.DB_DIALECT);
+
 sequelize.authenticate()
   .then(() => {
     console.log('Conexión establecida exitosamente.');
