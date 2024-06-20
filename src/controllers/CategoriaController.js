433677f57263c6cb16aa9f62f1df7322b3ca9f0d
Miguel Aragón <104454817+miguelaragonh@users.noreply.github.com>
2024-06-20 11:05:55 -0600

    Update CategoriaController.js

--- src/controllers/CategoriaController.js
+++ src/controllers/CategoriaController.js
@@ -3,9 +3,13 @@ const Categoria= require("../model/t_categoria");
 
 module.exports = {
 
-async getCategorias(req, res) {
-    let categoria = await Categoria.findAll();
-    res.json(categoria);
-  },
+  async getCategorias(req, res) {
+    try {
+      let categoria = await Categoria.findAll();
+      res.json(categoria);
+    } catch (error) {
+      res.status(500).json({ message: "Error al obtener las categorías", error: error.message });
+    }
+  }
 
 };
