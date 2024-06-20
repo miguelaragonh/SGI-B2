57b3da86b02c360ab4ee39e940eed12329c0c475
Miguel Aragón <104454817+miguelaragonh@users.noreply.github.com>
2024-06-19 22:56:02 -0600

    Update config.js

--- src/db/config.js
+++ src/db/config.js
@@ -6,6 +6,16 @@ module.exports = new Sequelize(
   process.env.DB_PASSWORD || null,
   {
     host: process.env.DB_HOST || "127.0.0.1",
-    dialect: process.env.DB_DIALECT || "mysql"
+    dialect: process.env.DB_DIALECT || "mysql",
+    // Ajustar los tiempos de espera
+    dialectOptions: {
+      connectTimeout: 60000, // 60 segundos
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
   }
-); 
+);
