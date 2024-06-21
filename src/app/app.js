0cf3e5f79552aecaed2c9667fd15cc16c0267e15
Miguel Aragón <104454817+miguelaragonh@users.noreply.github.com>
2024-06-21 01:01:56 -0600

    Update app.js

--- src/app/app.js
+++ src/app/app.js
@@ -4,7 +4,9 @@ const cors = require('cors');
 const router = require("../router/router")
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin:'*',
+    credentials : true}));
 app.use(morgan("dev"))
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
